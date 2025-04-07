d9b073f6d9f6a4489429a46dce17a59fbd971b98
Tyler Geiger <124940675+TylerGeiger513@users.noreply.github.com>
2025-04-07 18:07:47 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -28,13 +28,20 @@ bs.size = "20GB"
 
 node.addService(pg.Execute(shell="sh", command=f"""
 #!/bin/bash
-USER=$(getent passwd 1000 | cut -d: -f1)
-mkdir -p /home/$USER/.ssh
-echo '{params.CLOUDLAB_SSH_PUB_KEY}' > /home/$USER/.ssh/authorized_keys
-chmod 600 /home/$USER/.ssh/authorized_keys
-chown -R $USER:$USER /home/$USER/.ssh
-cat /home/$USER/.ssh/authorized_keys > /local/logs/authorized_keys.txt
-echo "KEY_INJECTED" > /local/logs/ssh_injection_status.txt
+USER=tg996676
+for i in {{1..10}}; do
+    if id "$USER" >/dev/null 2>&1; then
+        mkdir -p /home/$USER/.ssh
+        echo '{params.CLOUDLAB_SSH_PUB_KEY}' > /home/$USER/.ssh/authorized_keys
+        chmod 600 /home/$USER/.ssh/authorized_keys
+        chown -R $USER:$USER /home/$USER/.ssh
+        cat /home/$USER/.ssh/authorized_keys > /local/logs/authorized_keys.txt
+        echo "KEY_INJECTED" > /local/logs/ssh_injection_status.txt
+        break
+    fi
+    sleep 3
+done
 """))
+
 
 pc.printRequestRSpec(request)
